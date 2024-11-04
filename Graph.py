0021842ecb9b9f9f5dcdb76154c0c3a2d7b29690
Umut Yunusoğlu <umut.yunusoglu@sabanciuniv.edu>
2024-11-04 11:14:27 +0300

    _create_adjacency_matrix funciton of  Graph Class in Graph.py completed. Added additional documentation to Graph class. Set to naming convention of to vertex,edge from node,edge.

--- Graph.py
+++ Graph.py
@@ -1,9 +1,10 @@
 import dataclasses
-from typing import Any,Set,Iterable,Tuple,List
-
+from collections import defaultdict
+from typing import Any,Set,Iterable,Tuple,List,DefaultDict
+import numpy as np
 
 @dataclasses.dataclass
-class Node():
+class Vertex():
     value:Any
     is_visited:bool
 
@@ -14,52 +15,92 @@ class Graph():
     Graph Data Structure
     
         Protected Variables:
-            _vertices: Set[Node]: Set of Vertices
-            _edges:List[Tuple[Node,Node]]: List of edges of graph, where each edge is a Node,Node pair
+            _vertices: Set[Vertex]: Set of Vertices
+            _edges:List[Tuple[Vertex,Vertex]]: List of edges of graph, where each edge is a Vertex,Vertex pair
   
     """
 
 
 
-    def __init__(self,vertices:Iterable[Node],edges:List[Tuple[Node,Node]]):
+    def __init__(self,vertices:Iterable[Vertex],edges:List[Tuple[Vertex,Vertex]]):
         
         """
-        Constructor:
+        Constructor
         Params:
-            vertices:Iterable[Node] -> List of nodes of graph, duplicate nodes will be eliminated
-            edges:List[Tuple[Node,Node]] -> List of node pairs, representing the edges of graph
+            vertices:Iterable[Vertex] -> List of vertices of graph, duplicate vertices will be eliminated
+            edges:List[Tuple[Vertex,Vertex]] -> List of vertex pairs, representing the edges of graph
 
         Raises Exception When:
-            An node of an edge doesn't exist in node set
+            An node of an vertex doesn't exist in vertice set
             
+        Functionality:
+            Initialises Following Variables:
+                Protected:
+                    _vertices:List[Vertex] -> List of Unique Vertices 
+                        TODO: While implementing adding a nev vertex check uniqueness
+
+                    _edges:DefaultDict[Vertex,List[Vertex]] -> Lookup table for neighbours of a vertex
+
+                    _size:int -> Number of vertices
+
+                    _num_edges:int ->number of edges between vertices
             """
         
-        self._vertices:Set[Node] = set(vertices)
-        self._edges:List[Tuple[Node,Node]]=[]
+        self._vertices:List[Vertex] = [vertex for vertex in set(vertices)]
+        self._edges:DefaultDict[Vertex,List[Vertex]]=defaultdict(list)
 
-        for edge in self._edges:
+        for edge in edges:
             
             if not(edge[0] in self._vertices and edge[1] in self._vertices):
-                raise Exception("An End Point of Edge Doesnt Exist in Vertice Set")
+                raise Exception("An End Point of Vertex Doesnt Exist in Vertex Set")
             
-            self._edges.append(edge)
+            self._edges[edge[0]].append(edge[1])
 
 
+        self._size:int=len(self._vertices)
+        self._num_edges:int=len(self._edges)
+
+        self._create_adjacency_matrix()
 
     def _create_adjacency_matrix(self)->None:
         
         """
-        A function to create adjacency matrix representation of graph.
+        A helper function to create adjacency matrix representation of graph during constructer call.
+        Params:
+            None
 
-        _adjacency_matrix: np.ndarray[int]
+        Functionality
+        creates adjacency matrix:
+            _adjacency_matrix: np.ndarray[int]
 
         """
 
 
+        adjacency_matrix=np.zeros([self._size,self._size])
 
+        for vertex in self._vertices:
+            current_index = self._vertices.index(vertex)
 
+            for neigbour in self._edges[vertex]:
+                neigbour_index=self._vertices.index(neigbour)
 
-        pass
+                adjacency_matrix[current_index][neigbour_index]=1
+
+        self._adjacency_matrix=adjacency_matrix
     
+        
 
-    
+
+if(__name__=="__main__"):
+
+    vertices=[1,2,3,4]
+    edges=[(1,2),(1,4),
+            (2,1),(2,2),(2,3),(2,4),
+            (3,4),
+            (4,1),(4,2)]
+
+
+    g=Graph(vertices,edges)
+
+    print(g._edges)
+    print(g._adjacency_matrix)   
